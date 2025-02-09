b773fbeaf5b516e20a1d925c0c30cf14473c324d
ealpha072 <ealpha072@gmail.com>
2025-02-09 22:08:21 +0300

    Last checkpoint for 09-02-2025

--- app/config/database.php
+++ app/config/database.php
@@ -11,6 +11,7 @@ class Database {
         try {
             $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Connected to database";
         } catch (PDOException $exception) {
             echo "Database connection error: " . $exception->getMessage();
         }
---
+++ app/models/Employee.php
@@ -0,0 +1,11 @@
+<?php
+require_once __DIR__ . '../../../core/Model.php';
+
+class EmployeeModel extends Model{
+    public function testDb(){
+        
+    }
+}
+
+
+?>
