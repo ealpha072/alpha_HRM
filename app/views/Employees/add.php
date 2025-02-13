b91379c866c4f6110d37f5356f9b5f894e65d7d0
ealpha072 <ealpha072@gmail.com>
2025-02-13 22:21:42 +0300

    Add employee form done

--- app/views/Employees/add.php
+++ app/views/Employees/add.php
@@ -1,4 +1,4 @@
-<div class="container-fluid">
+<div class="">
     <div class="card">
         <div class="card-header">
             <div class="float-left">
@@ -8,122 +8,107 @@
         <div class="card-body">
             <!--form for admitting new student-->
             <form action="" method="POST" enctype="multipart/form-data">
-                <!--Academic info card-->
-                <div class="card academic_info">
-                    <div class="card-header">
-                        <p>Academic Info</p>
-                    </div>
-                    <div class="card-body">
-                        <div class="form-group row">
-                                <label for="admission-number" class="col-sm-2 col-form-label col-form-label-sm">Adm Num<sup>*</sup></label>
-                                <div class="col-sm-4">
-                                    <input type="number" placeholder="Adm Number" class="form-control form-control-sm" name="adm_num" required>
-                                </div>
-                                <label for="form" class="col-sm-2 col-form-label col-form-label-sm">Form<sup>*</sup></label>
-                                <div class="col-sm-4">
-                                    <select name="form" id="" class="form-control form-control-sm" required>
-                                        <option value=""  disabled selected="">--Form--</option>
-                                    </select>
-                                </div>
-                        </div>
-
-                        <div class="form-group row">
-                                <label for="stream" class="col-sm-2 col-form-label col-form-label-sm">Stream<sup>*</sup></label>
-                                <div class="col-sm-4">
-                                    <select name="stream" id="" class="form-control form-control-sm" required>
-                                        <option value="" disabled selected="">--Stream--</option>
-                                    </select>
-                                </div>
-
-                                <label for="hostel" class="col-sm-2 col-form-label col-form-label-sm">Hostel<sup>*</sup></label>
-                                <div class="col-sm-4">
-                                    <select name="hostel" id="" class="form-control form-control-sm" required>
-                                        <option value="" disabled selected="">--Hostel--</option>
-                                    </select>
-                                </div>
-                        </div>
-                        <div class="form-group row">
-                            <label for="admission-date" class="col-sm-2 col-form-label col-form-label-sm">Date</label>
-                            <div class="col-sm-4">
-                                <input type="text" disabled value="" class="form-control form-control-sm" name= "adm-date">
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!--END OF CARD-->
-
                 <!--personal card info-->
-                <div class="card personal_info">
-                    <div class="card-header">
-                        <p>Personal Info</p>
-                    </div>
-                    <div class="card-body">
-                        <div class="form-group row">
+                <div class="form-div personal_info">
 
-                            <label for="first-name" class="col-sm-2 col-form-label col-form-label-sm">First Name<sup>*</sup></label>
-                            <div class="col-sm-4">
-                                <input type="text" placeholder="First Name" class="form-control form-control-sm" name="first-name" required>
-                            </div>
+                    <h6>Personal Info</h6>
 
-                            <label for="middle-name" class="col-sm-2 col-form-label col-form-label-sm">Middle Name<sup>*</sup></label>
-                            <div class="col-sm-4">
-                                <input type="text" placeholder="Middle Name" class="form-control form-control-sm" name="middle-name" required>
-                            </div>
+                    <div class="form-group row">
+                        <label for="first-name" class="col-sm-2 col-form-label col-form-label-sm">Name<sup>*</sup></label>
+                        <div class="col-sm-4">
+                            <input type="text" class="form-control form-control-sm" name="first-name" required>
+                            <small id="emailHelp" class="form-text text-muted">First name</small>
                         </div>
 
-                        <div class="form-group row">
-                            <label for="last-name" class="col-sm-2 col-form-label col-form-label-sm">Last Name</label>
-                            <div class="col-sm-4">
-                                <input type="text" placeholder="Last Name" class="form-control form-control-sm" name="last-name">
-                            </div>
-
-                            <label for="gender" class="col-sm-2 col-form-label col-form-label-sm">Gender<sup>*</sup></label>
-                            <div class="col-sm-4">
-                                <select name="gender" id="" class="form-control form-control-sm" required>
-                                    <option value="" disabled selected="">--Choose Gender--</option>
-                                    <option value="male">Male</option>
-                                    <option value="female">Female</option>
-                                </select>
-                            </div>
+                        <div class="col-sm-4">
+                            <input type="text" class="form-control form-control-sm" name="middle-name" required>
+                            <small id="emailHelp" class="form-text text-muted">Last name</small>
                         </div>
                     </div>
+
+                    <div class="form-group row">
+                        <label for="last-name" class="col-sm-2 col-form-label col-form-label-sm">Personal Email</label>
+                        <div class="col-sm-4">
+                            <input type="email" class="form-control form-control-sm" name="personalemail">
+                        </div>
+
+                        <label for="gender" class="col-sm-2 col-form-label col-form-label-sm">Gender<sup>*</sup></label>
+                        <div class="col-sm-4">
+                            
+                            <select name="gender" id="" class="form-control form-control-sm" required>
+                                <option value="" disabled selected="">--Gender--</option>
+                                <option value="male">Male</option>
+                                <option value="female">Female</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    <div class="form-group row">
+                        <label for="phone-number" class="col-sm-2 col-form-label col-form-label-sm">Phone Number <sup>*</sup></label>
+                        <div class="col-sm-4">
+                            <input type="tel" placeholder="Phone Number" class="form-control form-control-sm" name="pphone-number" required maxlength="13">
+                            <small id="emailHelp" class="form-text text-muted">Phone number must be 13 digits (+2547XXXXXXXX)</small>
+                        </div>
+
+                        <label for="phone-number" class="col-sm-2 col-form-label col-form-label-sm">Photo <sup>*</sup></label>
+                        <div class="col-sm-4">
+                            <input type="file" class="form-control form-control-sm" name="pphone-number">
+                        </div>
+
+                        
+                    </div>
+
                 </div>
                 <!-- END OF CARD-->
 
                 <!--conatct info card-->
-                <div class="card contact_info">
-                    <div class="card-header">
-                        <p>Contact(Student's Parent/Guardian) Info</p>
-                    </div>
-                    <div class="card-body">
-                        <div class="form-group row">
+                <div class="form-div work_info">
 
-                            <label for="parent-name" class="col-sm-2 col-form-label col-form-label-sm">Full Name<sup>*</sup></label>
-                            <div class="col-sm-4">
-                                <input type="text" placeholder="Firstname Midname Lastname" class="form-control form-control-sm" name="pfirst-name" required>
-                            </div>
+                    <h6>Work Information</h6>
+                    <div class="form-group row">
 
-                            <label for="email" class="col-sm-2 col-form-label col-form-label-sm">Email <sup>*</sup></label>
-                            <div class="col-sm-4">
-                                <input type="email" placeholder="Email Address" class="form-control form-control-sm" name="pemail" required="">
-                                <small id="emailHelp" class="form-text text-muted">name@gmail.com</small>
-                            </div>
+                        <label for="parent-name" class="col-sm-2 col-form-label col-form-label-sm">Employee ID<sup>*</sup></label>
+                        <div class="col-sm-4">
+                            <input type="text" placeholder="" class="form-control form-control-sm" name="pfirst-name" required>
                         </div>
 
-                        <div class="form-group row">
-                            <label for="phone-number" class="col-sm-2 col-form-label col-form-label-sm">Phone Number <sup>*</sup></label>
-                            <div class="col-sm-4">
-                                <input type="tel" placeholder="Phone Number" class="form-control form-control-sm" name="pphone-number" required maxlength="13">
-                                <small id="emailHelp" class="form-text text-muted">Phone number must be 13 digits (+2547XXXXXXXX)</small>
-                            </div>
-                        </div>
-                        <hr>
-                        <div class="submit">
-                            <button class="btn btn-primary" type="submit" name="add-student" id="addnewstudent"><i class="fa fa-plus-circle fa-sm"></i> Add Student</button>
+                        <label for="email" class="col-sm-2 col-form-label col-form-label-sm">Employee Type <sup>*</sup></label>
+                        <div class="col-sm-4">
+                            <input type="email" class="form-control form-control-sm" name="pemail" required="">
                         </div>
                     </div>
+
+                    <div class="form-group row">
+                        <label for="last-name" class="col-sm-2 col-form-label col-form-label-sm">Personal Email</label>
+                        <div class="col-sm-4">
+                            <input type="email" class="form-control form-control-sm" name="personalemail">
+                        </div>
+
+                        <label for="email" class="col-sm-2 col-form-label col-form-label-sm">Employee Level <sup>*</sup></label>
+                        <div class="col-sm-4">
+                            <input type="email" class="form-control form-control-sm" name="pemail" required="">
+                        </div>
+
+                    </div>
+
+                    <div class="form-group row">
+                        <label for="last-name" class="col-sm-2 col-form-label col-form-label-sm">Date of Joining</label>
+                        <div class="col-sm-4">
+                            <input type="date" class="form-control form-control-sm" name="personalemail">
+                        </div>
+
+                        <label for="email" class="col-sm-2 col-form-label col-form-label-sm">Department <sup>*</sup></label>
+                        <div class="col-sm-4">
+                            <input type="email" class="form-control form-control-sm" name="pemail" required="">
+                        </div>
+
+                    </div>
+                    <hr>
+                    <div class="submit">
+                        <button class="btn btn-primary" type="submit" name="add-student" id="addnewstudent"><i class="fa fa-plus-circle fa-sm"></i> Add Student</button>
+                    </div>
+
                 </div>
-                <!--end of card-->
             </form>
             <!--END OF FORM-->
         </div>
