9319b886a58a086350197c85a649ba7b9af33363
ealpha072 <ealpha072@gmail.com>
2025-02-20 20:30:26 +0300

    Form for leave management

--- app/controllers/EmployeeController.php
+++ app/controllers/EmployeeController.php
@@ -7,7 +7,4 @@ class EmployeeController extends Controller{
     }
 
 }
-
-
-
 ?>
--- app/views/Leave/add.php
+++ app/views/Leave/add.php
@@ -14,28 +14,17 @@
                     <h6>Leave Info</h6>
 
                     <div class="form-group row">
-                        <label for="first-name" class="col-sm-2 col-form-label col-form-label-sm">Name<sup>*</sup></label>
+                        <label for="first-name" class="col-sm-2 col-form-label col-form-label-sm">Employee Name<sup>*</sup></label>
                         <div class="col-sm-4">
                             <input type="text" class="form-control form-control-sm" name="first_name" required>
-                            <small id="" class="form-text text-muted">First name</small>
-                        </div>
-
-                        <div class="col-sm-4">
-                            <input type="text" class="form-control form-control-sm" name="last_name" required>
-                            <small id="emailHelp" class="form-text text-muted">Last name</small>
                         </div>
                     </div>
 
                     <div class="form-group row">
-                        <label for="last-name" class="col-sm-2 col-form-label col-form-label-sm">Personal Email<sup>*</sup></label>
-                        <div class="col-sm-4">
-                            <input type="email" class="form-control form-control-sm" name="personal_email">
-                        </div>
-
-                        <label for="gender" class="col-sm-2 col-form-label col-form-label-sm">Gender<sup>*</sup></label>
+                        <label for="last-name" class="col-sm-2 col-form-label col-form-label-sm">Leave Type<sup>*</sup></label>
                         <div class="col-sm-4">
                             <select name="gender" id="" class="form-control form-control-sm" required>
-                                <option value="" disabled >--Gender--</option>
+                                <option value="" disabled >--Type--</option>
                                 <option value="male" selected>Male</option>
                                 <option value="female">Female</option>
                             </select>
@@ -43,17 +32,20 @@
                     </div>
 
                     <div class="form-group row">
-                        <label for="phone-number" class="col-sm-2 col-form-label col-form-label-sm">Phone Number <sup>*</sup></label>
+                        <label for="phone-number" class="col-sm-2 col-form-label col-form-label-sm">Start Date <sup>*</sup></label>
                         <div class="col-sm-4">
-                            <input type="tel" placeholder="Phone Number" class="form-control form-control-sm" name="phone_number" required maxlength="13">
-                            <small id="emailHelp" class="form-text text-muted">Phone number must be 13 digits (+2547XXXXXXXX)</small>
+                            <input type="date" placeholder="Phone Number" class="form-control form-control-sm" name="phone_number">
                         </div>
-
-                        <label for="photo" class="col-sm-2 col-form-label col-form-label-sm">Photo</label>
+                    </div>
+                    <div class="form-group row">
+                        <label for="phone-number" class="col-sm-2 col-form-label col-form-label-sm">End Date <sup>*</sup></label>
                         <div class="col-sm-4">
-                            <input type="file" class="form-control form-control-sm" name="photo" value="profile.jpg">
-                            <input type="hidden" name="photo_dummy" value="profile.jpg">
+                            <input type="date" placeholder="Phone Number" class="form-control form-control-sm" name="phone_number">
                         </div>
+                    </div>
+                    <hr>
+                    <div class="submit">
+                        <button class="btn btn-primary" name="add_employee" type="submit" id="add_employee"><i class="fa fa-plus-circle fa-sm"></i> Book Leave</button>
                     </div>
 
                 </div>
